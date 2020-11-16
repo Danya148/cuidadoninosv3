8521c8dcba93ac48e937335da128222bd3ae19b0
Danya Contreras <danyagomes028@outlook.com>
2020-11-15 19:55:23 -0500

    Nuevo

---
+++ Pag.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-widht,initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Cuidado de niños para madres que trabajan</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <br>
+      <center><svg class="bi bi-emoji-laughing" width="200px" height="200px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path fill-rule="evenodd" d="M12.331 9.5a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5z"/>
+        <path d="M7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5z"/>
+      </svg></center>
+
+      <p>
+          <center><h1>¡Su pago fue realizado con exito!</h1></center>
+          <br>
+          <center><h2>Puede contactarnos para mayor informacion.</h2></center>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ PagPrin.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-widht,initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>"Cuidado de niños para madres que trabajan"</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        
+    </head>
+    <body>
+          <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-interval="2500">
+                <img src="imgG2.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item" data-interval="2000">
+                <img src="imgG1.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item" data-interval="2000">
+                <img src="imgG8.jpg" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+            <div class="top-nav wow" style="background-color: paleturquoise;">
+                <div class="container">
+                    <div class="navbar-header logo">   
+                          <ul class="nav navbar">
+                           <li>
+                               <a href="#servicio1" class="scroll">Sobre nosotros</a>
+                           </li>
+                           <li>
+                            <a href="#servicio2" class="scroll">Servicio</a>
+                           </li>
+                           <li>
+                            <a href="#servicio3" class="scroll">Nuestras instalaciones</a>
+                           </li>
+                          </ul>
+                          <div class="clearfix"></div>
+                    </div>
+                    </div>
+                </div>
+                <br>
+            
+        <p id="servicio1">
+            <center><h1><p class="bg-warning">Sobre nosotros</p></h1></center>
+        </p>   
+        <br>
+        <p id="form">
+        <div class='container-fluid'></div>
+         <div class='row'>
+          <div class='col-md-0'></div><div class='col-md-4'><img src="imgG5.jpg" height="250px" width="300px" HSPACE="150" VSPACE="20" ></div><div class='col-md-0'></div><div class='col-md-7 '><center><br><br><h3>En nuestra empresa encontraras servicios para el cuidado de niños de madres que trabajan, tenemos personas especializadas en el cuidado de sus hijos para que tengan la confianza de dejarlos bajo nuestro cuidado, cada una de las personas que se encuentra trabajando en nuestra guarderia a tomado un curso sobre el cuidado correcto de los niños y medidadas de prevencion a accidentes que puedan ocurrir, todo esto para que madres como ustedes puedan dejar a sus hijos en manos de nosotros como especialistas con la confianza de que seran bien cuidados y atendidos para que se sientan como en casa, espero que confien en nosotros y nos contraten.</h3></center></div>
+         </div> 
+        </p>
+        <br>
+        <br>
+        <p id="servicio2">
+            <center><h1><p class="bg-info">Servicio</p></h1></center>
+        </p>
+        <br>
+        <p id="acer">
+        <div class='container-fluid'></div>
+         <div class='row'>
+         <div class='col-md-0'></div><div class='col-md-7 '><center><h3>El servicio que le ofrecemos es el cuidado de su niño o niños, entre los beneficios que se ofrecen se encuentra el horario flexible para que usted pueda elegir cuantas horas quiere que su niño quede bajo nuestro cuidado, el servicio que se le ofrece tiene un precio aproximado entre $2,000 y $3,000 depende de las horas de cuidado que requiera el menor, en este servicio vienen incluidas las comidas de sus hijos y todos lo servicios como luz y agua, se les brinda el mejor higiene y comidas nutritivas para los niños como sus meriendas si asi se requiere.</h3></center></div><div class='col-md-0'></div><div class='col-md-5'><img src="imgG6.jpg" height="250px" width="420px" HSPACE="150" VSPACE="20" ></div>
+        </div>
+        </p>
+        <br>
+        <center><a href="InformacionTra.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Contratar servicio</a></center>        
+        <br>
+        <p id="servicio3">
+            <center><h1><p class="bg-success">Sobre nuestras instalaciones</p></h1></center>
+        </p>
+        <p id="Log">
+        <div class='container-fluid'></div>
+         <div class='row'>
+          <div class='col-md-0'></div><div class='col-md-5'><img src="imgG7.jpg" height="250px" width="400px" HSPACE="150" VSPACE="20" ></div><div class='col-md-0'></div><div class='col-md-6'><center><h3>Nuestras instalaciones son las mejor equipadas de la ciudad, ya que contamos con cuartos de cuidado para niños de todas las edades, asi como una cocina en donde se preparan todos los alimentos de los niños en el momento de la comida, contamos con un comedor en el cual los niños pueden comer e interactuar con los demas niños bajo la responsabilidad de sus niñeras en curso, contamos con un area de juegos para que los niños puedan divertirse la cual es muy segura ya que tiene cosas especificas para que los niños no se lastimen, todas estas aulas cuenta con aire acondicionado, y sus hijos siempre estaran bajo el cuidado de personas especializadas en su cuidado.</h3></center></div>
+         </div>
+         <br>
+         <br>
+        </p>
+        <br>
+        <br>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
